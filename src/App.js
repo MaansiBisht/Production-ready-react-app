9aa4bbb7b7a96dcee81acc40b81d0db72c45d38d
Maansi Bisht <maansibisht466@gmail.com>
2023-05-13 19:39:29 +0530

    Starter app code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import UploadForm from "./components/UploadForm";
+import "./App.css";
+
+const photos = [
+  'https://picsum.photos/id/1001/200/200'
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [count, setCount] = useState()
+  const [inputs, setInputs] = useState({ title: null, file: null, path: null});
+  const [items, setItems] = useState(photos);
+  const [isCollapsed, collapse] = useState(false);
+  const toggle = () => collapse(!isCollapsed);
+  const handleOnChange = (e) => {
+    if (e.target.name === 'file') {
+      setInputs({ ...inputs, file: e.target.files[0], path: URL.createObjectURL(e.target.files[0])})
+    } else {
+      setInputs({...inputs, title: e.target.value})
+    }
+   
+  }
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    setItems([inputs.path,...items])
+  }
 
+  useEffect(() => {
+    setCount(`you have ${items.length} image${items.length > 1 ? 's': ''}`)
+  }, [items])
+  return (
+    <>
+      <Navbar />
+      <div className="container text-center mt-5">
+        <button className="btn btn-success float-end" onClick={toggle}>{isCollapsed ? 'Close' : '+ Add'}</button>
+        <div className="clearfix mb-4"></div>
+        <UploadForm 
+          isVisible={isCollapsed}
+          onChange={handleOnChange}
+          onSubmit={handleOnSubmit}
+        />
+        {count}
+        <h1>Gallery</h1>
+
+        <div className="row">
+        {items.map((photo, index) => <Card key={index} src={photo}/>)}
+        </div>
+      </div>
+    </>
+  );
+ 
+}
 export default App;
