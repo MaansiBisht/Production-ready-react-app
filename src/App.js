0f337bfb69f52cdca86e9bb1d0864271e493afb1
Maansi Bisht <maansibisht466@gmail.com>
2023-05-14 13:24:02 +0530

    use of context api for global management of variables.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,25 @@
-import { useState, useEffect } from "react";
-import Navbar from "./components/Navbar";
+import { useState, useEffect, useReducer , useMemo, useContext} from "react";
 import Card from "./components/Card";
-import UploadForm from "./components/UploadForm";
+import Layout from "./components/Layout";
+import { Context } from "./context";
+
 import "./App.css";
 
-const photos = [
-  'https://picsum.photos/id/1001/200/200'
-]
-
 function App() {
-  const [count, setCount] = useState()
-  const [inputs, setInputs] = useState({ title: null, file: null, path: null});
-  const [items, setItems] = useState(photos);
-  const [isCollapsed, collapse] = useState(false);
-  const toggle = () => collapse(!isCollapsed);
-  const handleOnChange = (e) => {
-    if (e.target.name === 'file') {
-      setInputs({ ...inputs, file: e.target.files[0], path: URL.createObjectURL(e.target.files[0])})
-    } else {
-      setInputs({...inputs, title: e.target.value})
-    }
-   
-  }
-  const handleOnSubmit = (e) => {
-    e.preventDefault()
-    setItems([inputs.path,...items])
-  }
+  const {dispatch, state} = useContext(Context)
+  const count = useMemo(() => {
+    return `you have ${state.items.length} image${state.items.length > 1 ? 's': ''}`
+  }, [state.items])
 
-  useEffect(() => {
-    setCount(`you have ${items.length} image${items.length > 1 ? 's': ''}`)
-  }, [items])
   return (
     <>
-      <Navbar />
-      <div className="container text-center mt-5">
-        <button className="btn btn-success float-end" onClick={toggle}>{isCollapsed ? 'Close' : '+ Add'}</button>
-        <div className="clearfix mb-4"></div>
-        <UploadForm 
-          isVisible={isCollapsed}
-          onChange={handleOnChange}
-          onSubmit={handleOnSubmit}
-        />
+      <Layout>
+        <h1 className="text-center">Gallery</h1>
         {count}
-        <h1>Gallery</h1>
-
         <div className="row">
-        {items.map((photo, index) => <Card key={index} src={photo}/>)}
+        {state.items.map((item, index) => <Card key={index} {...item}/>)}
         </div>
-      </div>
+    </Layout>
     </>
   );
  
--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,9 +1,39 @@
-const UploadForm = ({ isVisible, onChange, onSubmit }) => {
+import { useMemo,  useContext } from "react"
+import { Context } from "../context"
+
+const Preview = () => {
+  const { state } = useContext(Context)
+  const { inputs } = state
+  return (
+    inputs.path && <div
+      className="rounded p-1 m-5"
+      style={{
+        width: "30%",
+        height: "300px",
+        backgroundImage: `url(${inputs.path}`,
+        backgroundSize: "cover",
+      }}
+    ></div>
+  );
+};
+
+const UploadForm = () => {
+  const { dispatch, state } = useContext(Context)
+  const handleOnChange = (e) => dispatch({ type: 'setInputs', payload: { value: e}})
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    dispatch({ type: 'setItem'})
+    dispatch({ type: "collapse", payload: { bool: false }})
+  }
+    const isDisabled = useMemo(() => {
+      return !!Object.values(state.inputs).some(input => !input)
+    }, [state.inputs])
     return (
-      isVisible && <>
+      state.isCollapsed && <>
       <p className="display-6 text-center mb-3">Upload Stock Image</p>
       <div className="mb-5 d-flex align-items-center justify-content-center">
-        <form className="mb-2" style={{ textAlign: "left" }} onSubmit={onSubmit}>
+      <Preview {...state.inputs} />
+      <form className="mb-2" style={{ textAlign: "left" }} onSubmit={handleOnSubmit}>
           <div className="mb-3">
             <input
               type="text"
@@ -11,15 +41,16 @@ const UploadForm = ({ isVisible, onChange, onSubmit }) => {
               name="title"
               placeholder="title"
               aria-describedby="text"
-              onChange={onChange}
+              onChange={handleOnChange}
             />
           </div>
           <div className="mb-3">
-            <input type="file" className="form-control" name="file" onChange={onChange}/>
+            <input type="file" className="form-control" name="file" onChange={handleOnChange}/>
           </div>
           <button
             type="submit"
             className="btn btn-success float-end"
+            disabled={isDisabled}
           >
             Save changes
           </button>
