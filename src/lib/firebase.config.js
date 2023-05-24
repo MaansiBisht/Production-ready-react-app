efa08ff897616db23d6805fbad498a221def9a97
Maansi Bisht <maansibisht466@gmail.com>
2023-05-24 19:37:19 +0530

    removing keys and tokens

--- src/lib/firebase.config.js
+++ src/lib/firebase.config.js
@@ -7,21 +7,13 @@ To initialize firebase below, go yo  https://firebase.google.com/docs/web/setup?
 */
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBlzGqVB4E8yh0H84dpo7VqOiTKdPmL5aA",
-    authDomain: "image-stocks-5272f.firebaseapp.com",
-    projectId: "image-stocks-5272f",
-    storageBucket: "image-stocks-5272f.appspot.com",
-    messagingSenderId: "093215775549",
-    appId: "1:1093215775549:web:a0d8fae54703f6af70f354"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_API_KEY,
-//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//     projectId: process.env.REACT_APP_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_APP_ID
-// };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
